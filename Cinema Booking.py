97f49c0d0375b4384db4f4b70eef1464db8bd34d
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-05-12 21:40:20 +0100

    Create Cinema Booking.py

---
+++ Cinema Booking.py
@@ -0,0 +1,71 @@
+tickets = int(input("How many tickets for your family do you require?"))
+print("Please enter the ages for each ticket")
+
+def checkTicketNumbers(tickets):
+    try:
+        tickets = int(tickets)
+    except ValueError:
+        print("***Invalid Data Input***")
+        print("You must enter digits like 2 or 3")
+        return 0
+    if tickets > 5 or tickets < 2:
+        print("You can only book 2 to 5 tickets and number")
+        return 0
+    else:
+        return tickets
+
+ticketsValidated = checkTicketNumbers(tickets)
+
+ages = []
+
+i = 1
+while i <= ticketsValidated:
+    ages.append(int(input(f"What age is the family member for ticket {i}: ")))
+    i+=1
+
+def ageVerification(ages):
+    noBabies = True
+    accompanyingAdults = False
+    for x in ages:
+        if x < 2:
+            noBabies = False
+            break
+        if x > 19:
+            accompanyingAdults = True
+            
+    if accompanyingAdults and noBabies:
+        return True
+    else:
+        return False
+
+ageValid = ageVerification(ages)            
+        
+
+def priceCalc(ages):
+    if ageValid != True:
+        return 
+    Adults = 0
+    Teenagers = 0
+    Children = 0
+    Total = 0
+    for x in ages:
+        if x > 19:
+            Adults +=1
+            Total += 14.5
+        elif x > 10:
+            Teenagers +=1
+            Total += 10
+        else:
+            Children +=1
+            Total += 5
+    return [Adults, Teenagers, Children, Total] 
+
+infoArray = priceCalc(ages)
+totalCost = "£{:,.2f}".format(infoArray[3])
+
+print("Thank You")
+print("TICKET PRICE DETAILS are as follows:")
+print(f"Number of Adults = {infoArray[0]} at £14.50 each")
+print(f"Number of Teenagers = {infoArray[1]} at £10.00 each")
+print(f"Number of Childrens = {infoArray[2]} at £5.00 each")
+print(f"The total cost of these tickets are {totalCost}")
